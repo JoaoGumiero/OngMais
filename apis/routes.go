da51945e4bf33d6a8918b9b4d6347e1cb825576c
JoaoGumiero <joaogumiero1410@gmail.com>
2024-07-18 19:45:38 -0300

    Route fixing

--- apis/routes.go
+++ apis/routes.go
@@ -6,10 +6,10 @@ import (
 
 func RegisterRoutes(router *mux.Router) {
 	// Register routes for the voluntary entity
-	router.HandleFunc("/voluntaries", CreateVoluntary).Methods("POST")
-	router.HandleFunc("/voluntaries/{id}", GetVoluntary).Methods("GET")
-	router.HandleFunc("/voluntaries/{id}", UpdateVoluntary).Methods("PUT")
-	router.HandleFunc("/voluntaries/{id}", DeleteVoluntary).Methods("DELETE")
+	router.HandleFunc("/voluntary", CreateVoluntary).Methods("POST")
+	router.HandleFunc("/voluntary/{id}", GetVoluntary).Methods("GET")
+	router.HandleFunc("/voluntary/{id}", UpdateVoluntary).Methods("PUT")
+	router.HandleFunc("/voluntary/{id}", DeleteVoluntary).Methods("DELETE")
 	router.HandleFunc("/states", getStates).Methods("GET")
 	router.HandleFunc("/cities", getCities).Methods("GET")
 }
